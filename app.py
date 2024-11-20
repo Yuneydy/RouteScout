a18783a678f11a8bf763e4c548ebbab2b2df35a2
Madeleine Hansen <mh115@wellesley.edu>
2024-11-19 20:21:33 -0500

    fixing upload route form

--- app.py
+++ app.py
@@ -157,11 +157,9 @@ def upload_route(uid):
                 routeTcx = request.form.get("route_tcx")
                 levelRun = request.form.get("difficulty")
                 mile = request.form.get("distance")
-                startLoc = request.form.get("starting_location")
                 startTow = request.form.get("starting_town")
-                endLoc = request.form.get("finishing_location")
                 endTow = request.form.get("finishing_town")
-                outBack = request.form.get("out_and_back")
+                outAndBack = request.form.get("outBack")
                 bathr = request.form.get("bathrooms")
                 bathDescrip = request.form.get("bathroom_location")
                 waterFount = request.form.get("water")
@@ -170,8 +168,8 @@ def upload_route(uid):
                 numMile = float(mile)
         
                 curs = dbi.cursor(conn)
-                query = 'INSERT INTO route_info(routeName, route_description, route_tcx, level, mileage, starting_location, starting_town, finishing_location, finishing_town, out_and_back, bathroom, bathroom_description, water_fountain, fountain_description, addedBy) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-                curs.execute(query, (routeDescrip, routeTcx, levelRun, numMile, startLoc, startTow, endLoc, endTow, outBack, bathr, bathDescrip, waterFount, fountDescrip, uid))
+                query = 'INSERT INTO route_info(name, route_description, route_tcx, level, mileage, starting_location, starting_town, finishing_location, finishing_town, out_and_back, bathroom, bathroom_description, water_fountain, fountain_description, addedBy) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                curs.execute(query, (routeName, routeDescrip, routeTcx, levelRun, numMile, None, startTow, None, endTow, outAndBack, bathr, bathDescrip, waterFount, fountDescrip, uid))
                 conn.commit()
                 
                 flash('Your route has been submitted! Thank you!')
